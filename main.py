2172831820237f3b43eefd52a8e1da2a6fa50c23
Andre <andre.katsuragawa@gmail.com>
2021-04-14 20:55:37 -0300

    updated nav bar and homepage when logged in

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -76,7 +76,7 @@ def register():
         # login the new user
         login_user(new_user)
         return redirect(url_for("secrets"))
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -93,13 +93,13 @@ def login():
 
         login_user(user)
         return redirect(url_for("secrets"))
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @login_required
 @app.route("/secrets")
 def secrets():
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=current_user.is_authenticated)
 
 
 @login_required
